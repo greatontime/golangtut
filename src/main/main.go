3cd7b6936a9c3ce52bd2c48fa0a8ca18c7874a21
greatontime <pathomphong1144@gmail.com>
2016-02-15 07:50:44 +0700

    All source

---
+++ src/main/main.go
@@ -0,0 +1,10 @@
+package main
+import "net/http"
+
+func main(){
+	http.HandleFunc("/",someFunc)
+	http.ListenAndServe(":8080",nil)
+}
+func someFunc(w http.ResponseWriter, req *http.Request){
+	w.Write([]byte("Hello world"))
+}
